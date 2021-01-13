9a4ab7b7b58560e5307150cafa7e0d3159059653
gaukharB <berdibekova.gaukhar@gmail.com>
2021-01-13 14:17:09 +0600

    fragments

---
+++ app/src/main/java/kz/kolesateam/dreamapp/Hello/HelloFragment.kt
@@ -0,0 +1,36 @@
+package kz.kolesateam.dreamapp.Hello
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kz.kolesateam.dreamapp.R
+
+class HelloFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hello, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController= Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.helloNextButton).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.helloNextButton ->navController!!.navigate(R.id.action_helloFragment_to_mainMenuFragment)
+        }
+    }
+}
---
+++ app/src/main/java/kz/kolesateam/dreamapp/Hello/MainActivity.kt
@@ -0,0 +1,12 @@
+package kz.kolesateam.dreamapp.Hello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kz.kolesateam.dreamapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
